0dcd760faf40f53b3d9f792c6addb98110e52b09
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-06-08 21:29:35 -0300

    Added automatic input focus change

--- src/components/LetterInputs.tsx
+++ src/components/LetterInputs.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 type LetterInputsProps = {
   disable: boolean;
   classNames: string[];
@@ -5,17 +7,58 @@ type LetterInputsProps = {
 };
 
 function LettersInputs(props: LetterInputsProps) {
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+  const handleChange = (index: number, value: string) => {
+    props.saveTypedLetters(index, value);
+
+    if (value.length === 1) {
+      let allInputsFilled = false;
+
+      for (let i = index; i < 5; i++) {
+        if (!inputRefs.current[i]?.value) {
+          inputRefs.current[i]?.focus();
+          break;
+        }
+
+        allInputsFilled = true;
+      }
+
+      if (allInputsFilled) {
+        for (let i = 0; i < 5; i++) {
+          if (!inputRefs.current[i]?.value) {
+            inputRefs.current[i]?.focus();
+            break;
+          }
+        }
+      }
+    }
+  };
+
   return (
     <div>
       {[0, 1, 2, 3, 4].map(index => (
         <input
+          key={index}
           className={props.classNames[index]}
+          ref={element => {
+            inputRefs.current[index] = element;
+          }}
           type="text"
           name={`char${index}`}
           maxLength={1}
           minLength={1}
           disabled={props.disable}
-          onChange={event => props.saveTypedLetters(index, event.target.value)}
+          onChange={event => handleChange(index, event.target.value)}
+          onKeyDown={event => {
+            if (
+              event.key === "Backspace" &&
+              !event.currentTarget.value &&
+              index > 0
+            ) {
+              inputRefs.current[index - 1]?.focus();
+            }
+          }}
         />
       ))}
     </div>
